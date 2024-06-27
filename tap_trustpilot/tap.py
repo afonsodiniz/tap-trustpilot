b7809a0088c3c683463946cb106d0c601e8728bf
Afonso Diniz <afonso@rauva.com>
2024-06-27 10:55:41 +0100

    chore: initial developments

--- tap_trustpilot/tap.py
+++ tap_trustpilot/tap.py
@@ -14,32 +14,20 @@ class TapTrustpilot(Tap):
 
     name = "tap-trustpilot"
 
-    # TODO: Update this section with the actual config values you expect:
     config_jsonschema = th.PropertiesList(
-        th.Property(
-            "auth_token",
-            th.StringType,
-            required=True,
-            secret=True,  # Flag config as protected.
-            description="The token to authenticate against the API service",
-        ),
-        th.Property(
-            "project_ids",
-            th.ArrayType(th.StringType),
-            required=True,
-            description="Project IDs to replicate",
-        ),
-        th.Property(
-            "start_date",
-            th.DateTimeType,
-            description="The earliest record date to sync",
-        ),
         th.Property(
             "api_url",
             th.StringType,
-            default="https://api.mysample.com",
+            default="https://api.trustpilot.com/v1",
             description="The url for the API service",
         ),
+        th.Property(
+            "api_key",
+            th.StringType,
+            required=True,
+            secret=True,  # Flag config as protected.
+            description="The key to authenticate against the API service",
+        )
     ).to_dict()
 
     def discover_streams(self) -> list[streams.TrustpilotStream]:
@@ -49,8 +37,7 @@ class TapTrustpilot(Tap):
             A list of discovered streams.
         """
         return [
-            streams.GroupsStream(self),
-            streams.UsersStream(self),
+            streams.Reviews(self)
         ]
 
 
